822015a26ce7196c3f81aded6e5bb98abfb82552
killergui <fucv@163.com>
2019-03-21 01:22:08 +0800

    自己写的查找引用以及资源丢失的脚本
    暂时存放在这里

---
+++ Editor/FindSceneReferences.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+public class FindSceneReferences
+{
+
+    [MenuItem("Assets/Find References Scene", false, 100)]
+
+    static private void Find()
+    {
+        EditorSettings.serializationMode = SerializationMode.ForceText;
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        string dataPath = Application.dataPath + "/Resources/#Scene#scenes";
+
+        if (!string.IsNullOrEmpty(path))
+        {
+            string guid = AssetDatabase.AssetPathToGUID(path);
+            List<string> withoutExtensions = new List<string>() { ".prefab", ".unity", ".mat", ".asset" };
+            string[] files = Directory.GetFiles(dataPath, "*.*", SearchOption.AllDirectories)
+                .Where(s => withoutExtensions.Contains(Path.GetExtension(s).ToLower())).ToArray();
+            int startIndex = 0;
+
+            EditorApplication.update = delegate ()
+            {
+                string file = files[startIndex];
+
+                bool isCancel = EditorUtility.DisplayCancelableProgressBar("匹配资源中", file, (float)startIndex / (float)files.Length);
+
+                if (Regex.IsMatch(File.ReadAllText(file), guid))
+                {
+                    Debug.Log(file, AssetDatabase.LoadAssetAtPath(GetRelativeAssetsPath(file), typeof(Object)));
+                }
+
+                startIndex++;
+                if (isCancel || startIndex >= files.Length)
+                {
+                    EditorUtility.ClearProgressBar();
+                    EditorApplication.update = null;
+                    startIndex = 0;
+                    Debug.Log("#Scene#scenes匹配结束");
+                    dataPath = Application.dataPath + "/Resources/_levels";
+
+                    if (!string.IsNullOrEmpty(path))
+                    {
+                        guid = AssetDatabase.AssetPathToGUID(path);
+
+                        files = Directory.GetFiles(dataPath, "*.*", SearchOption.AllDirectories)
+                            .Where(s => withoutExtensions.Contains(Path.GetExtension(s).ToLower())).ToArray();
+                        startIndex = 0;
+
+                        EditorApplication.update = delegate ()
+                        {
+                            file = files[startIndex];
+
+                            isCancel = EditorUtility.DisplayCancelableProgressBar("匹配资源中", file, (float)startIndex / (float)files.Length);
+
+                            if (Regex.IsMatch(File.ReadAllText(file), guid))
+                            {
+                                Debug.Log(file, AssetDatabase.LoadAssetAtPath(GetRelativeAssetsPath(file), typeof(Object)));
+                            }
+
+                            startIndex++;
+                            if (isCancel || startIndex >= files.Length)
+                            {
+                                EditorUtility.ClearProgressBar();
+                                EditorApplication.update = null;
+                                startIndex = 0;
+                                Debug.Log("_levels匹配结束");
+                            }
+
+                        };
+                    }
+                }
+
+            };
+        }
+    }
+
+    [MenuItem("Assets/Find References Scene", true)]
+    static private bool VFind()
+    {
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        return (!string.IsNullOrEmpty(path));
+    }
+
+    static private string GetRelativeAssetsPath(string path)
+    {
+        return "Assets" + Path.GetFullPath(path).Replace(Path.GetFullPath(Application.dataPath), "").Replace('\\', '/');
+    }
+}
---
+++ Editor/MissingReferencesFinder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+
+public class MissingReferencesFinder : EditorWindow
+{
+    [MenuItem("Tools/Show Missing", false, 1000)]
+    public static void MissingSpritesInAssets()
+    {
+        var allAssets = AssetDatabase.GetAllAssetPaths();
+        var objs = allAssets.Select(a => AssetDatabase.LoadAssetAtPath(a, typeof(GameObject)) as GameObject).Where(a => a != null).ToArray();
+
+        FindMissingReferences("Project", objs);
+    }
+
+    private static void FindMissingReferences(string context, GameObject[] objects)
+    {
+        foreach (var go in objects)
+        {
+            Component[] cps = go.GetComponentsInChildren<Component>(true);
+
+            foreach (var c in cps)
+            {
+                if (!c)
+                {
+                    Debug.LogError("Missing Component in GO: " + FullPath(go), go);
+                    continue;
+                }
+
+                SerializedObject so = new SerializedObject(c);
+                var sp = so.GetIterator();
+
+                while (sp.NextVisible(true))
+                {
+                    if (sp.propertyType == SerializedPropertyType.ObjectReference)
+                    {
+                        if (sp.objectReferenceValue == null
+                            && sp.objectReferenceInstanceIDValue != 0)
+                        {
+                            ShowError(context, go, c.GetType().Name, ObjectNames.NicifyVariableName(sp.name));
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private const string err = "Missing Ref in: [{3}]{0}. Component: {1}, Property: {2}";
+
+    private static void ShowError(string context, GameObject go, string c, string property)
+    {
+        if (property == "Data")
+        {
+            Debug.LogError(string.Format(err, FullPath(go), c, property, context), go);
+        }
+    }
+
+    private static string FullPath(GameObject go)
+    {
+        return go.transform.parent == null
+            ? go.name
+                : FullPath(go.transform.parent.gameObject) + "/" + go.name;
+    }
+}
